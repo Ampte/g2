4cf9a91a1e4f11de1d7b68c3a6859015af91e8f0
Ampte B Marak <amptebmarak@gmail.com>
2025-03-27 13:24:34 +0530

    Major changes

---
+++ src/components/card.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+
+const Card = () => {
+    return(
+        <>
+        <div className='product-card'>
+            <div className='card'>
+                <img src={"https://m.media-amazon.com/images/I/612W9n19MzL._SY695_.jpg"} className='product-image'/>
+                <p>Product name</p>
+                <p>Price</p>
+            </div>
+            <div className='card'>
+                <img src={"https://m.media-amazon.com/images/I/612W9n19MzL._SY695_.jpg"} className='product-image'/>
+                <p>Product name</p>
+                <p>Price</p>
+            </div>
+            <div className='card'>
+                <img src={"https://m.media-amazon.com/images/I/612W9n19MzL._SY695_.jpg"} className='product-image'/>
+                <p>Product name</p>
+                <p>Price</p>
+            </div>
+        </div>
+        <div className='product-card'>
+            <div className='card'>
+                <img src={"https://m.media-amazon.com/images/I/612W9n19MzL._SY695_.jpg"} className='product-image'/>
+                <p>Product name</p>
+                <p>Price</p>
+            </div>
+            <div className='card'>
+                <img src={"https://m.media-amazon.com/images/I/612W9n19MzL._SY695_.jpg"} className='product-image'/>
+                <p>Product name</p>
+                <p>Price</p>
+            </div>
+            <div className='card'>
+                <img src={"https://m.media-amazon.com/images/I/612W9n19MzL._SY695_.jpg"} className='product-image'/>
+                <p>Product name</p>
+                <p>Price</p>
+            </div>
+        </div>
+        <div className='product-card'>
+            <div className='card'>
+                <img src={"https://m.media-amazon.com/images/I/612W9n19MzL._SY695_.jpg"} className='product-image'/>
+                <p>Product name</p>
+                <p>Price</p>
+            </div>
+            <div className='card'>
+                <img src={"https://m.media-amazon.com/images/I/612W9n19MzL._SY695_.jpg"} className='product-image'/>
+                <p>Product name</p>
+                <p>Price</p>
+            </div>
+            <div className='card'>
+                <img src={"https://m.media-amazon.com/images/I/612W9n19MzL._SY695_.jpg"} className='product-image'/>
+                <p>Product name</p>
+                <p>Price</p>
+            </div>
+        </div>
+        </>
+    );
+};
+export default Card;
--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,10 +1,61 @@
 import React from 'react'
 import '/src/App.css'
+import Card from './card';
 
 const Home = () => {
+
+    const open = () => {
+        const sidebar = document.querySelector('.sidebar');
+        sidebar.style.display = 'block';
+    };
+
+    const close = () => {
+        const sidebar = document.querySelector('.sidebar');
+        sidebar.style.display = 'none';
+    };
+
     return(
         <>
-        <h1>HELLO WORLD</h1>
+        <div className='home'>
+            <div className='header'>
+                <button className='bar' onClick={open}><i class="fa-solid fa-bars-staggered" style={{fontSize: 'large'}}></i></button>
+                <h1>Ches Thrift</h1>
+                <button className='user-circle'>
+                <i class="fa-solid fa-circle-user" style={{
+                    fontSize: '30px'
+                }}></i>
+                </button>
+            </div>
+            <div className='search'>
+                <form>
+                    <input type='text' className='search-value' placeholder='Search...'/>
+                    <button type='submit'>Search</button>
+                </form>
+            </div>
+            <div className='sidebar'>
+                <div className='logo'>
+                <h1>Ches Thrift</h1>
+                <button className='xmark' onClick={close}>
+                <i class="fa-solid fa-xmark"style={{
+                    fontSize:'large'
+                }}></i>
+                </button>
+                </div>
+                <div className='links'>
+                <button className='home-btn'>Home</button>
+                <button className='catego-btn'>Categories</button>
+                <button className='fav-btn'>Favourites</button>
+                <button className='exchange-btn'>Exchange</button>
+                <button className='order-btn'>My orders</button>
+                </div>
+            </div>
+            <div className='main'>
+                <Card/>
+            </div>
+            <div className='footer'>
+                <p>&copy; Ches Thrift.</p>
+            </div>
+        </div>
         </>
     );
 };
